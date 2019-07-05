4e3a2d51f19b8b87e5d6fdc98aa8286acf2a6402
Paulo Borba <pauloesb@gmail.com>
2019-07-05 16:30:10 -0300

    Created test suit for Urls Controller

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -4,8 +4,19 @@ RSpec.describe UrlsController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      get :home
+      get :index
       expect(response).to have_http_status(:success)
+    end
+
+    it "contains Url model instance" do
+      get :index
+      expect(assigns(:url)).to be_a_new(Url)
+    end
+  end
+
+  describe "POST #create" do
+    it "create a new Url" do
+      expect{ post :create, params: {url: attributes_for(:url)}}.to change(Url, :count).by(1)
     end
   end
 
