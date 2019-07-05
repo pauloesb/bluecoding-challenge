74fe5abfa13e5fb319eb1fadac5fe8b4b765976f
Paulo Borba <pauloesb@gmail.com>
2019-07-05 16:29:09 -0300

    Created Urls controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,4 +1,16 @@
 class UrlsController < ApplicationController
   def index 
+    @url = Url.new
+  end
+
+  def create
+    @url = Url.create(url_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:original_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root to: 'urls#index'
+
+  resources :urls, only: :create 
 end
