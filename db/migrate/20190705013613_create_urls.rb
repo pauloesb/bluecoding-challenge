b85d20d41dd8dc22cf2cb51e180d594d51705093
Paulo Borba <pauloesb@gmail.com>
2019-07-04 22:44:10 -0300

    Created Url model

---
+++ db/migrate/20190705013613_create_urls.rb
@@ -0,0 +1,12 @@
+class CreateUrls < ActiveRecord::Migration[5.2]
+  def change
+    create_table :urls do |t|
+      t.string :origina_url
+      t.string :short_url
+      t.string :title
+      t.integer :access
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/urls.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :url do
+    origina_url { "MyString" }
+    short_url { "MyString" }
+    title { "MyString" }
+    access { 1 }
+  end
+end
