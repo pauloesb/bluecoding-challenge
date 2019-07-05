01974cbc29fdc9d94196a2d4a58567370b920d75
Paulo Borba <pauloesb@gmail.com>
2019-07-05 19:16:53 -0300

    Configuring Gritter to flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def alerts
+    alert = (flash[:alert] || flash[:error] || flash[:notice])
+
+    if alert
+      alert_generator alert, "Attention"
+    end
+  end
+
+  def alert_generator msg, title
+    js add_gritter(msg, title: title, sticky: false)
+  end
 end
