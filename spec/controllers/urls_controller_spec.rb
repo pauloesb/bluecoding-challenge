e96652e1bdf8f6c28d48df70f8c8bea14c697a93
Paulo Borba <pauloesb@gmail.com>
2019-07-05 16:51:39 -0300

    Created test to verify redirect of generated short url

--- spec/controllers/urls_controller_spec.rb
+++ spec/controllers/urls_controller_spec.rb
@@ -18,6 +18,12 @@ RSpec.describe UrlsController, type: :controller do
     it "create a new Url" do
       expect{ post :create, params: {url: attributes_for(:url)}}.to change(Url, :count).by(1)
     end
+
+    it "redirect short url from the original url" do
+      url = FactoryBot.create(:url)
+      post :create, params: {url: attributes_for(:url)}
+      expect(response).to redirect_to Url.find_by_original_url(url.original_url).short_url
+    end
   end
 
 end
