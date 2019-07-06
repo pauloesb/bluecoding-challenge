f3494b2ccd8a05617544cf2616c10730a3083b0b
Paulo Borba <pauloesb@gmail.com>
2019-07-06 20:22:59 -0300

    Added htmlentities to decode html from title sometimes

--- app/workers/title_retriever_worker.rb
+++ app/workers/title_retriever_worker.rb
@@ -1,12 +1,14 @@
+require 'htmlentities'
+
 class TitleRetrieverWorker
     include Sidekiq::Worker
     include HTTParty
 
     def perform(id)
         url = Url.find(id) 
-        response = HTTParty.get(url.original_url)
+        response = HTTParty.get(url.original_url, timeout: 30)
         if response.to_s.match(/<title ?.*>(.*)<\/title>/)
-          url.update(title: $1)
+          url.update(title: HTMLEntities.new.decode($1))
         else
           url.update(title: "Title not found!")
         end
