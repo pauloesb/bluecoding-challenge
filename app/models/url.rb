09e0db37f0c538b3a869c5c7740590bbf639132c
Paulo Borba <pauloesb@gmail.com>
2019-07-06 20:22:00 -0300

    Created scope to show the top 100 most accessed pages

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,4 +3,5 @@ class Url < ApplicationRecord
     validates :original_url, presence: true
     validates_format_of :original_url,
         with: /\A(?:(?:http(s)*):\/\/|\b(?:[a-z\d]+\.))(?:(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))?\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))?\z/
+    scope :top_100, -> { order(access: :desc, original_url: :asc).limit(100) }
 end
