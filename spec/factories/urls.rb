99ccdb857d74fdc3219d11c65c6163e224527ddc
Paulo Borba <pauloesb@gmail.com>
2019-07-06 17:57:20 -0300

    Created Complete Url factory for testing purpose

--- spec/factories/urls.rb
+++ spec/factories/urls.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :url do
     original_url { "http://www.google.com" }
+
+    factory :complete_url do
+      id { 1 }
+      original_url { "http://yahoo.com" }
+      short_url { "a" }
+    end
   end
 end
