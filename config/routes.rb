96128349ed18e01d6d4fbd06ee52a9662407d4d9
Paulo Borba <pauloesb@gmail.com>
2019-07-06 20:22:15 -0300

    Added the route to top 100

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'urls#index'
-
+  get '/top-100', to: 'urls#top100', as: :top_100
   get '/:short_url', to: 'urls#redirect', as: :redirect
   get '/result/:short_url', to: 'urls#result', as: :result
   resources :urls, only: [:create] 
