5339f87720236b53ddc134558d1e5bf51d30d2e1
Paulo Borba <pauloesb@gmail.com>
2019-07-07 15:05:43 -0300

    Created static methods for lengthurl for control purpose

--- app/models/length_url.rb
+++ app/models/length_url.rb
@@ -1,2 +1,18 @@
 class LengthUrl < ApplicationRecord
+    class << LengthUrl
+        def increase
+            length = instance
+            length.update(size: length.size+1)
+        end
+
+        def size
+            instance.size
+        end
+
+        private
+
+        def instance
+            LengthUrl.first || LengthUrl.create
+        end
+    end
 end
