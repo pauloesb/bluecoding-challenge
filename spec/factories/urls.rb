496aee34e99726ec62b58057fa30e59b34313b44
Paulo Borba <pauloesb@gmail.com>
2019-07-07 15:06:50 -0300

    Created more factories to multiple tests for url object

--- spec/factories/urls.rb
+++ spec/factories/urls.rb
@@ -7,5 +7,17 @@ FactoryBot.define do
       original_url { "http://yahoo.com" }
       short_url { "a" }
     end
+
+    factory :empty_url do
+      original_url { "" }
+    end
+
+    factory :wrong_url do
+      original_url { "http://thisismysite" }
+    end
+
+    factory :dummy_url do
+      original_url { "http://my.dummy.site.com" }
+    end
   end
 end
