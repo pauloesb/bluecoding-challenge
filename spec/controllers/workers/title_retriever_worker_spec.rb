7559273f82aa23717705883ca9354e3c51f5fa4a
Paulo Borba <pauloesb@gmail.com>
2019-07-07 12:15:29 -0300

    Fixing TitoleRetreverWorker test

--- spec/controllers/workers/title_retriever_worker_spec.rb
+++ spec/controllers/workers/title_retriever_worker_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe TitleRetrieverWorker, type: :job do
     it "retrieve title for a url" do
         url = FactoryBot.create(:complete_url)
         Sidekiq::Testing.inline! do
-            TitleRetrieverWorker.perform_async(url.id)
+            TitleRetrieverWorker.perform_async(url.short_url)
             expect(Url.last.title).to eq("Yahoo Brasil")
         end
     end
