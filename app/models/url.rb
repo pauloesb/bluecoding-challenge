0387eafc2921eb7ca4a9b381dcca6c988d33582f
Paulo Borba <pauloesb@gmail.com>
2019-07-07 15:06:21 -0300

    Improved regex to capture better links

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,6 +2,6 @@ class Url < ApplicationRecord
     include UrlConcerns
     validates :original_url, presence: true
     validates_format_of :original_url,
-        with: /\A(?:(?:http(s)*):\/\/|\b(?:[a-z\d]+\.))(?:(?:[^\s()<>]+|\((?:[^\s()<>]+|(?:\([^\s()<>]+\)))?\))+(?:\((?:[^\s()<>]+|(?:\(?:[^\s()<>]+\)))?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))?\z/
+        with: /\A(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})\z/
     scope :top_100, -> { order(access: :desc, original_url: :asc).limit(100) }
 end
