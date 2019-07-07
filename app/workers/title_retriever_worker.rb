709630067765c6aefbfcc42c2c56d6510c193e60
Paulo Borba <pauloesb@gmail.com>
2019-07-07 00:34:54 -0300

    Removing unnecessary logic inside worker

--- app/workers/title_retriever_worker.rb
+++ app/workers/title_retriever_worker.rb
@@ -5,21 +5,19 @@ class TitleRetrieverWorker
     include HTTParty
     sidekiq_options retry: 2
 
-    attr_accessor :url
-
     def perform(short_url)
       begin
-        @url = Url.find_by_short_url(short_url)
+        url = Url.find_by_short_url(short_url)
         response = HTTParty.get(url.original_url, timeout: 30)
         if response.to_s.match(/<title?\s?.*>(.*)<\/title>/)
-          @url.update(title: parse_treatment($1))
+          url.update(title: parse_treatment($1))
         else
-          @url.update(title: "Title not found!")
+          url.update(title: "Title not found!")
         end
       rescue HTTParty::RedirectionTooDeep
-        @url.update(title: "Error - Too Many Redirections!")
+        url.update(title: "Error - Too Many Redirections!")
       rescue Errno::ECONNREFUSED
-        @url.update(title: "Error - Site Unavailable!")
+        url.update(title: "Error - Site Unavailable!")
       end
     end
 
