0eae9f2ba07d313a34bd1d95f597bfe0003e6f91
Paulo Borba <pauloesb@gmail.com>
2019-07-07 18:54:08 -0300

    Ordening scope by title as well

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,5 +3,5 @@ class Url < ApplicationRecord
     validates :original_url, presence: true
     validates_format_of :original_url,
         with: /\A(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})\z/
-    scope :top_100, -> { order(access: :desc, original_url: :asc).limit(100) }
+    scope :top_100, -> { order(access: :desc, title: :asc, original_url: :asc).limit(100) }
 end
