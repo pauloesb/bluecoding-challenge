0205a11bf35ed505408c73e01c2a423bd58a2c7e
Paulo Borba <pauloesb@gmail.com>
2019-07-07 00:33:54 -0300

    Implemented error handling for the TitleRetrieverWorker

--- app/workers/title_retriever_worker.rb
+++ app/workers/title_retriever_worker.rb
@@ -3,14 +3,34 @@ require 'htmlentities'
 class TitleRetrieverWorker
     include Sidekiq::Worker
     include HTTParty
+    sidekiq_options retry: 2
 
-    def perform(id)
-        url = Url.find(id) 
+    attr_accessor :url
+
+    def perform(short_url)
+      begin
+        @url = Url.find_by_short_url(short_url)
         response = HTTParty.get(url.original_url, timeout: 30)
-        if response.to_s.match(/<title ?.*>(.*)<\/title>/)
-          url.update(title: HTMLEntities.new.decode($1))
+        if response.to_s.match(/<title?\s?.*>(.*)<\/title>/)
+          @url.update(title: parse_treatment($1))
         else
-          url.update(title: "Title not found!")
+          @url.update(title: "Title not found!")
         end
+      rescue HTTParty::RedirectionTooDeep
+        @url.update(title: "Error - Too Many Redirections!")
+      rescue Errno::ECONNREFUSED
+        @url.update(title: "Error - Site Unavailable!")
+      end
+    end
+
+    private
+
+    # Reason to use this enconding so the job doesn't get stuck on
+    # reading non-utf characters
+    def parse_treatment(parse)
+      content = parse.b
+      content.encode!('UTF-8', 'ISO-8859-1')
+      content.encoding
+      HTMLEntities.new.decode(content)
     end
 end
