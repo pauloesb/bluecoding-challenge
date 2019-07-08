9bd13d9dc41c062894e33fff1fb0d78c77fa022d
Paulo Borba <pauloesb@gmail.com>
2019-07-08 20:02:45 -0300

    Fixed bug

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,6 +1,6 @@
 class Url < ApplicationRecord
     include UrlConcerns
-    validates_uniqueness_of :shortgs_url
+    validates_uniqueness_of :shorts_url
     validates :original_url, presence: true
     validates_format_of :original_url,
         with: /\A(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})\z/
